33e79007c5331577cfa6181a018f43a06d04e546
landon <landon.seebart@gmail.com>
2018-03-11 16:18:54 -0500

    set inital variabels and game start conditions

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+'use strict';
+
+console.log('hello world!');
+
+// Declare variables
+
+const goal = Math.floor((Math.random() * 120) + 1);
+const firstButton = Math.floor((Math.random() * 12) + 1);
+const secondButton = Math.floor((Math.random() * 12) + 1);
+const thirdButton = Math.floor((Math.random() * 12) + 1);
+const fourthButton = Math.floor((Math.random() * 12) + 1);
+let score = 0;
+let wins = 0;
+let losses = 0;
+
+console.log(goal, firstButton, secondButton, thirdButton, fourthButton, score, wins, losses)
