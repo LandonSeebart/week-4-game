1c6cbe953f007f03175670ea0584bc6a3c5882b6
landon <landon.seebart@gmail.com>
2018-03-12 21:05:39 -0500

    finished javascript and jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,62 @@
 'use strict';
 // Declare variables
 
-let goal = Math.floor((Math.random() * 120) + 1);
-let firstButton = Math.floor((Math.random() * 12) + 1);
+let goal = getRandomNumber(120)
 let score = 0;
 let wins = 0;
 let losses = 0;
+let buttonValues = generateButtonValues();
 
-console.log(goal, score, firstButton, wins);
+// Generate array of button values
+function generateButtonValues() {
+  let result = [];
+  for (let i = 0; i < 4; i++) {
+    result.push(getRandomNumber(12));
+  }
+  return result;
+}
+
+// generates random number below n
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * n) + 1;
+}
 
 // Scoring function
-
-$("#button-one").click(function () {
-    score += firstButton;
+$('.selector').click(function() {
+    score += buttonValues[$(this).index()]
     $("#score").text(score);
 
     if (score === goal) {
-        wins ++;
+        wins++;
         $("#win-count").text(wins);
+        $('<div id="win-loss-banner">You Win!</div>').appendTo($("body"));
         console.log('you win!');
         gameEnd();
 
     } else if (score > goal) { 
-    losses ++;
+    losses++;
     $("#loss-count").text(losses);
+    $('<div id="win-loss-banner">You loose!</div>').appendTo($("body"));
     console.log("you loose!");
-    gameEnd ();
-
+    gameEnd();
     }
-});
+  })
+
+// Ends game
 
 const gameEnd = function(){
-    $("#button-one").prop("disabled", true);
+    $('.selector').prop("disabled", true);
     $('<input type="button" value="Start New Game" id="reset-button" />').appendTo($("body"));
     $("#reset-button").click(resetGame);
 }
 
 // game reset
-
-const resetGame = function (){
-    goal = Math.floor((Math.random() * 120) + 1);
-    firstButton = Math.floor((Math.random() * 12) + 1);
+const resetGame = function(){
+    goal = getRandomNumber(120) 
+    buttonValues = generateButtonValues();
     score = 0;
     $("#score").text(score);
-    $("#button-one").prop("disabled", false);
+    $('.selector').prop("disabled", false);
     $( "#reset-button" ).remove();
+    $( "#win-loss-banner" ).remove();
 }
-
-
-
-
